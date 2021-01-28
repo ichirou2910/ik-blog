2026e255b6a0aedf6841f1b7b6f1b6505c7580f4
Ichirou2910 <ichiroukeita2910@gmail.com>
2021-01-28 13:37:01 +0700

    Cleanup unused APIs

--- routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/user.controller");
-
-// Routes
-router.get("/", userController.getInfo);
-router.post("/authenticate", userController.login);
-
-module.exports = router;
